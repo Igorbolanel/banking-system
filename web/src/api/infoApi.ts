a4eb7777dc528c811fdf012f37ec39d7cd8b2700
igor <bolanel02@mail.ru>
2026-05-15 21:57:18 +0300

    Add news API client

--- web/src/api/infoApi.ts
+++ web/src/api/infoApi.ts
@@ -3,4 +3,8 @@ import type { NewsItem } from '../types/banking';
 import { API_URLS } from './config';
 import { request, withFallback } from './http';
 
-export const infoApi = { getNews() { return withFallback<NewsItem[]>(() => request(`${API_URLS.info}/api/news`), news); } };
+export const infoApi = {
+  getNews() {
+    return withFallback<NewsItem[]>(() => request(`${API_URLS.info}/api/news`), news);
+  },
+};
